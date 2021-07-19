4d900c7663644b7666e56b6742ff6d5a5dff68cb
mborin88 <57768638+mborin88@users.noreply.github.com>
2021-07-19 21:50:11 +0100

    Bluetooth test code
    UART serial transmission from ELEC1201_ C9 Lab

--- Motor/CarTest.c
+++ Motor/CarTest.c
@@ -7,13 +7,13 @@ void initMotor(){ //Initialise D0  D1, D2 as ouputs
 	DDRD |= _BV(1);	//Load 2
 	DDRD |= _BV(2);	//Load 3
 	
-	PORTD &= ~_BV(0);	//Initialise D0 off
-	PORTD |= _BV(1);	//Initialise D1 off
-	PORTD &= ~_BV(2);	//Initialise D2 off
+
 }
 
 int main(){
 	while(1){
-		initMotor();
+		PORTD &= ~_BV(0);	//Initialise D0 off
+		PORTD |= _BV(1);	//Initialise D1 off
+		PORTD &= ~_BV(2);	//Initialise D2 off
 	}
 }
